7a614eb6c8fe2958e02d7534c6b6a0452ba4802a
KaushalKrSoni <kaushal.ece21.com>
2024-03-14 18:22:05 +0530

    A2Q7 added

---
+++ A2Q7b.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int year;
+	printf("Enter the year :\n");
+	scanf("%d",&year);
+
+	if(year%400==0)
+			printf("%d is a Leap year\nTotal number of days in %d is= 366\n");
+
+	else if(year%4==0 && year%100!=0)
+			printf("%d is a Leap year\nTotal number of days in %d is= 366\n");
+
+	else
+		printf("%d is not a Leap year\nTotal number of days in %d is= 365\n");
+	
+	return 0;
+}
